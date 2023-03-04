fe60c7047796e7f31b57ebfdadb227944a665382
Lu <lutequi22@gmail.com>
2023-03-04 01:13:08 +0100

    Cat

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Vector2 bounds;
+
     private void Update()
     {
-        transform.position = new Vector2(transform.position.x + 1 * Time.deltaTime, transform.position.y);
+        MovePlayer();
+        CheckBounds();
+    }
+
+    private void MovePlayer()
+    {
+        Vector2 velocity = new Vector2();
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity.x = 1;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            velocity.x = -1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocity.y = -1;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            velocity.y = 1;
+        }
+
+        transform.Translate(velocity.normalized * speed * Time.deltaTime);
+
+    }
+
+    private void CheckBounds()
+    {
+        Vector2 position;
+
+        position.x = Mathf.Clamp(transform.position.x, -bounds.x, bounds.x);
+        position.y = Mathf.Clamp(transform.position.y, -bounds.y, bounds.y);
+
+        transform.position = position;
     }
 }
