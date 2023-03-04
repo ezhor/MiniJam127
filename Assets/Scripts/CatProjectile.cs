e54e9cf031e0abbd833eebc2e7df6a0307b7de0b
Ézhor Malkávar <ezhormalkavar@gmail.com>
2023-03-04 20:34:02 +0100

    Projectile logic half-made. Background prefab added.

---
+++ Assets/Scripts/CatProjectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CatProjectile : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] faces;
+
+    private void Start()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode shootKey;
+
+    [SerializeField]
+    private float shootDelay;
+
+    [SerializeField]
+    private GameObject projectilePrefab;
+
+    [SerializeField]
+    private Transform projectileSpawn;
+
+    private bool canShoot = true;
+
+
+    private void Update()
+    {
+        if (canShoot && Input.GetKeyDown(shootKey))
+        {
+            StartCoroutine(ShootCoroutine());
+        }
+    }
+
+    private IEnumerator ShootCoroutine()
+    {
+        canShoot = false;
+
+        Instantiate(projectilePrefab, projectileSpawn.position, Quaternion.identity);
+
+        yield return new WaitForSeconds(shootDelay);
+
+        canShoot = true;
+    }
+}
