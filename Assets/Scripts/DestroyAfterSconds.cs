61bef0ed84cb5df9662f961c44e686bc5b237649
Ézhor Malkávar <ezhormalkavar@gmail.com>
2023-03-05 15:57:33 +0100

    DogGhost SFX

---
+++ Assets/Scripts/DestroyAfterSconds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyAfterSconds : MonoBehaviour
+{
+    [SerializeField]
+    private float seconds;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(seconds);
+
+        Destroy(gameObject);
+    }
+}
