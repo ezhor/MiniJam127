6c8f72aa93e8ddc88b466a2bc3517665482f258d
Ézhor Malkávar <ezhormalkavar@gmail.com>
2023-03-05 18:40:26 +0100

    End

---
+++ Assets/Scripts/End.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            SceneManager.LoadSceneAsync("End");
+        }
+    }
+}
